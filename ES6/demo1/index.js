45287a11b34bfdab116948ad0e044a0a5976a162
吴忠 <wuzhong1598@qq.com>
2018-10-19 10:00:57 +0800

    add demo

---
+++ ES6/demo1/index.js
@@ -0,0 +1,17 @@
+// import util1 from './src/util1.js'
+// import { fn1, fn2 } from './src/util2.js'
+
+
+[1, 2, 3].map(item => item + 2)
+
+
+
+// "use strict";
+
+// // import util1 from './src/util1.js'
+// // import { fn1, fn2 } from './src/util2.js'
+
+
+// [1, 2, 3].map(function (item) {
+//   return item + 2;
+// });
